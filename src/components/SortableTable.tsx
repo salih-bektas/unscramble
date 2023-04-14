7ce6e8132b6dd2f446b098504b27897ef5c18455
Salih Bektas <salih.bektas@gmail.com>
2023-04-15 02:44:54 +0300

    arayüzdeki alignment sorunu giderildi

--- src/components/SortableTable.tsx
+++ src/components/SortableTable.tsx
@@ -1,5 +1,11 @@
-import React, { useState } from 'react';
-import { View, Text, TouchableOpacity, StyleSheet, ScrollView } from 'react-native';
+import React, { useState } from "react";
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  StyleSheet,
+  ScrollView,
+} from "react-native";
 
 type Column = {
   id: string;
@@ -8,11 +14,11 @@ type Column = {
 };
 
 type Data = {
-    word: string;
-    length: number;
-    score: number;
-    coloredHtml: string;
-    [key: string]: string | number;
+  word: string;
+  length: number;
+  score: number;
+  coloredHtml: string;
+  [key: string]: string | number;
 };
 
 type Props = {
@@ -21,43 +27,50 @@ type Props = {
 };
 
 export const SortableTable: React.FC<Props> = ({ columns, data }) => {
-  const [sortConfig, setSortConfig] = useState<{ key: string; direction: 'asc' | 'desc' } | null>(null);
+  const [sortConfig, setSortConfig] = useState<{
+    key: string;
+    direction: "asc" | "desc";
+  } | null>(null);
 
   const sortedData = React.useMemo(() => {
     const sortableData = [...data];
     if (sortConfig !== null) {
       sortableData.sort((a, b) => {
         if (a[sortConfig.key] < b[sortConfig.key]) {
-          return sortConfig.direction === 'asc' ? -1 : 1;
+          return sortConfig.direction === "asc" ? -1 : 1;
         }
         if (a[sortConfig.key] > b[sortConfig.key]) {
-          return sortConfig.direction === 'asc' ? 1 : -1;
+          return sortConfig.direction === "asc" ? 1 : -1;
         }
         return 0;
       });
     }
     return sortableData;
-}, [data, sortConfig]);
+  }, [data, sortConfig]);
 
-const requestSort = (key: string, sortable: boolean) => {
+  const requestSort = (key: string, sortable: boolean) => {
     if (!sortable) return;
-    let direction: 'asc' | 'desc' = 'asc';
-    if (sortConfig && sortConfig.key === key && sortConfig.direction === 'asc') {
-      direction = 'desc';
+    let direction: "asc" | "desc" = "asc";
+    if (
+      sortConfig &&
+      sortConfig.key === key &&
+      sortConfig.direction === "asc"
+    ) {
+      direction = "desc";
     }
     setSortConfig({ key, direction });
-};
+  };
 
-const renderColoredText = (coloredHtml: string) => {
+  const renderColoredText = (coloredHtml: string) => {
     const regex = /<span style="color: (.*?);">(.*?)<\/span>/g;
     let match;
     let lastIndex = 0;
     const elements: any[] = [];
-  
+
     while ((match = regex.exec(coloredHtml)) !== null) {
       const [fullMatch, color, text] = match;
       const prefix = coloredHtml.slice(lastIndex, match.index);
-  
+
       if (prefix) {
         elements.push(<Text key={`prefix-${match.index}`}>{prefix}</Text>);
       }
@@ -66,111 +79,110 @@ const renderColoredText = (coloredHtml: string) => {
           {text}
         </Text>
       );
-  
+
       lastIndex = match.index + fullMatch.length;
     }
-  
+
     const suffix = coloredHtml.slice(lastIndex);
     if (suffix) {
       elements.push(<Text key={`suffix-${lastIndex}`}>{suffix}</Text>);
     }
-  
+
     return <Text>{elements}</Text>;
   };
-  
-  
 
-return (
-    <ScrollView>
-      <View style={{ flexDirection: 'column' }}>
-        <View style={{ flexDirection: 'row' }}>
-          {columns.map((column, columnIndex) => (
-            <TouchableOpacity
-              key={column.id}
-              onPress={() => requestSort(column.id, column.sortable)}
-              style={{
-                width: columnIndex === 0 ? '50%' : '25%',
-                alignItems: 'center',
-                justifyContent: 'center',
-                padding: 10,
-                backgroundColor: 'lightgray',
-                borderWidth: 1,
-                borderColor: 'white',
-              }}
-            >
-                <Text>
-                    {column.label}{' '}
-                    {sortConfig && sortConfig.key === column.id
-                    ? sortConfig.direction === 'asc'
-                        ? '↑'
-                        : '↓'
-                    : ''}
-                </Text>
-            </TouchableOpacity>
-          ))}
-        </View>
-        {sortedData.map((row, index) => (
-          <View key={index} style={{ flexDirection: 'row' }}>
-            {columns.map((column, columnIndex) => (
-              <View
-                key={column.id}
-                style={{
-                  width: columnIndex === 0 ? '50%' : '25%',
-                  alignItems: 'center',
-                  justifyContent: 'center',
-                  padding: 10,
-                  borderWidth: 1,
-                  borderColor: 'white',
-                }}
-              >
-                {column.id === 'word' ? (
-                renderColoredText(row['coloredHtml'] as string)
-                ) : (
-                <Text>{row[column.id]}</Text>
-                )}
-              </View>
-            ))}
-          </View>
+  return (
+    <View style={{ flex: 1 }}>
+      <View style={{ flexDirection: "row", backgroundColor: "white" }}>
+        {columns.map((column, columnIndex) => (
+          <TouchableOpacity
+            key={column.id}
+            onPress={() => requestSort(column.id, column.sortable)}
+            style={{
+              width: columnIndex === 0 ? "50%" : "25%",
+              alignItems: "center",
+              justifyContent: "center",
+              padding: 10,
+              backgroundColor: "lightgray",
+              borderWidth: 1,
+              borderColor: "white",
+            }}
+          >
+            <Text>
+              {column.label}{" "}
+              {sortConfig && sortConfig.key === column.id
+                ? sortConfig.direction === "asc"
+                  ? "↑"
+                  : "↓"
+                : ""}
+            </Text>
+          </TouchableOpacity>
         ))}
       </View>
-    </ScrollView>
+      <ScrollView contentContainerStyle={{ flexGrow: 1 }}>
+        <View style={{ flexDirection: "column" }}>
+          {sortedData.map((row, index) => (
+            <View key={index} style={{ flexDirection: "row" }}>
+              {columns.map((column, columnIndex) => (
+                <View
+                  key={column.id}
+                  style={{
+                    width: columnIndex === 0 ? "50%" : "25%",
+                    alignItems: "center",
+                    justifyContent: "center",
+                    padding: 10,
+                    borderWidth: 1,
+                    borderColor: "white",
+                  }}
+                >
+                  {column.id === "word" ? (
+                    renderColoredText(row["coloredHtml"] as string)
+                  ) : (
+                    <Text>{row[column.id]}</Text>
+                  )}
+                </View>
+              ))}
+            </View>
+          ))}
+        </View>
+      </ScrollView>
+    </View>
   );
- };
- 
- const styles = StyleSheet.create({
-   container: {
-     flex: 1,
-   },
-   header: {
-     flexDirection: 'row',
-     borderBottomWidth: 1,
-     borderBottomColor: '#ccc',
-   },
-   columnHeader: {
-     flex: 1,
-     flexDirection: 'row',
-     alignItems: 'center',
-     padding: 8,
-   },
-   columnHeaderText: {
-     marginRight: 4,
-   },
-   row: {
-     flexDirection: 'row',
-   },
-   cell: {
-     flex: 1,
-     padding: 8,
-     justifyContent: 'center',
-     borderBottomWidth: 1,
-     borderBottomColor: '#ccc',
-   },
-   cellText: {
-     textAlign: 'center',
-   },
-   coloredWord: {
-     flexDirection: 'row',
-     justifyContent: 'center',
-   },
- });
- 
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  header: {
+    flexDirection: "row",
+    borderBottomWidth: 1,
+    borderBottomColor: "#ccc",
+  },
+  columnHeader: {
+    flex: 1,
+    flexDirection: "row",
+    alignItems: "center",
+    padding: 8,
+  },
+  columnHeaderText: {
+    marginRight: 4,
+  },
+  row: {
+    flexDirection: "row",
+  },
+  cell: {
+    flex: 1,
+    padding: 8,
+    justifyContent: "center",
+    borderBottomWidth: 1,
+    borderBottomColor: "#ccc",
+  },
+  cellText: {
+    textAlign: "center",
+  },
+  coloredWord: {
+    flexDirection: "row",
+    justifyContent: "center",
+  },
+});
